31882287f1a9682256982ef0b8c3a7508657bbb8
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-10-02 13:56:47 -0700

    after vid 6

---
+++ app/src/main/java/android/baltazarg/blog_reader/BlogPost.java
@@ -0,0 +1,12 @@
+package android.baltazarg.blog_reader;
+
+public class BlogPost {
+    public final String title;
+    public final String url;
+
+    public BlogPost(String title, String url) {
+        this.title=title;
+        this.url=url;
+
+    }
+}
