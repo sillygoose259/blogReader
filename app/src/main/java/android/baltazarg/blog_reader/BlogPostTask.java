eff0d3c7037d08832a70ffe90b7b613e440cf66d
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-10-03 13:38:08 -0700

    after vid 8

---
+++ app/src/main/java/android/baltazarg/blog_reader/BlogPostTask.java
@@ -0,0 +1,13 @@
+package android.baltazarg.blog_reader;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
+    @Override
+    protected JSONObject doInBackground(Activity... params) {
+        return null;
+    }
+}
