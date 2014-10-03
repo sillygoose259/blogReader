0ace1c8421c7ef0a13a19fe858e8f73821c988bd
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-10-03 13:55:14 -0700

    after vid 9

--- app/src/main/java/android/baltazarg/blog_reader/BlogPostTask.java
+++ app/src/main/java/android/baltazarg/blog_reader/BlogPostTask.java
@@ -2,12 +2,23 @@ package android.baltazarg.blog_reader;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
     @Override
     protected JSONObject doInBackground(Activity... params) {
+        try {
+            URL blogFeedURL = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10");
+        }
+
+        catch(MalformedURLException error) {
+                 Log.e("BlogPostTask", "Malformed URL" + error);
+        }
         return null;
     }
 }
