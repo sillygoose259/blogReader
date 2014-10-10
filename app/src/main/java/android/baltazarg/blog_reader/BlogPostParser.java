920c76eac919c0c24a8d708ee5d3ed128b3d0053
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-10-10 13:54:56 -0700

    after vid 15

--- app/src/main/java/android/baltazarg/blog_reader/BlogPostParser.java
+++ app/src/main/java/android/baltazarg/blog_reader/BlogPostParser.java
@@ -1,8 +1,11 @@
 package android.baltazarg.blog_reader;
 
+import android.util.Log;
+
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -20,9 +23,21 @@ public class BlogPostParser {
         }
 
         public JSONObject parse(InputStream inputStream) {
-            BufferedReader reader =  new BufferedReader(new InputStreamReader(inputStream));
-                StringBuilder builder = new StringBuilder();
-                JSONObject jsonObject =  null;
-               return null;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder builder = new StringBuilder();
+            JSONObject jsonObject = null;
+            String line;
+
+            try {
+                while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                }
+                    Log.i("BlogPostParser", "Input Stream String:" + builder.toString());
+
+            }
+                catch(IOException error) {
+                    Log.e("BlogPostParser", "IOException:" + error);
+                }
+            return jsonObject;
         }
 }
