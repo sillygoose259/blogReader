4360fffec3330c66db40f933670f39478b7e918e
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-10-14 13:58:23 -0700

    after vid 19

--- app/src/main/java/android/baltazarg/blog_reader/BlogPostParser.java
+++ app/src/main/java/android/baltazarg/blog_reader/BlogPostParser.java
@@ -10,10 +10,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class BlogPostParser {
         private static BlogPostParser parser;
-
+        public ArrayList<BlogPost> posts;
         private BlogPostParser() {
 
         }
@@ -50,4 +51,8 @@ public class BlogPostParser {
 
             return jsonObject;
         }
+
+    public void readFeed(JSONObject jsonObject) {
+
+    }
 }
