1f6491a3f40bbce6335ee14119b972be51875337
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-11-03 10:00:03 -0800

    after vid 36

---
+++ app/src/main/java/android/baltazarg/blog_reader/BlogWebActivity.java
@@ -0,0 +1,12 @@
+package android.baltazarg.blog_reader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlogWebActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_web);
+    }
+}
