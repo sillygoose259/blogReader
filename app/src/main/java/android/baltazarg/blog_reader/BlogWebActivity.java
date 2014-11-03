bdb8a4596bd3f1f610fb2e8c2d2dd156e8326751
Gregory Baltazar Avalos <sillygoose259@gmail.com>
2014-11-03 10:17:56 -0800

    after vid 38 entering intermediate

--- app/src/main/java/android/baltazarg/blog_reader/BlogWebActivity.java
+++ app/src/main/java/android/baltazarg/blog_reader/BlogWebActivity.java
@@ -1,12 +1,21 @@
 package android.baltazarg.blog_reader;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class BlogWebActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_web);
+
+        Intent intent = getIntent();
+        Uri blogUri = intent.getData();
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.loadUrl(blogUri.toString());
     }
 }
